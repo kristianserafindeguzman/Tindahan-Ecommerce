5f327c1fac50180a859f7923c70976019920daa4
gelo-gels <riangeloo17@gmail.com>
2026-07-19 22:14:51 +0800

    push frontend

---
+++ frontend/src/pages/IndexPage.vue
@@ -0,0 +1,22 @@
+<template>
+  <q-page class="flex flex-center">
+    <h4>{{ message }}</h4>
+  </q-page>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+import { api } from '../boot/axios'
+
+const message = ref('Loading...')
+
+onMounted(async () => {
+  try {
+    const { data } = await api.get('/test')
+    message.value = data.message
+  } catch (error) {
+    console.error(error)
+    message.value = 'Failed to connect to Laravel'
+  }
+})
+</script>
